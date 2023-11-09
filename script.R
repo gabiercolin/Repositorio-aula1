4c4a37336212e8b6f4f5d37bccaab8376f433770
Gabriele-AE <gaercolin@estudante.ufscar.br>
2023-11-09 16:31:37 -0300

    Mudando o script

--- script.R
+++ script.R
@@ -2,14 +2,5 @@
 
 #Esse código você só precisará rodar uma vez por máquina (por instalação)
 
-usethis::use_git_config(# Seu nome no GitHub
-  user.name = "Gabriele-AE",
-  # Seu email no GitHub
-  user.email = "gaercolin@estudante.ufscar.br")
-
-
-usethis::create_github_token()
-
-usethis::edit_r_environ()
-
-usethis::git_sitrep() verificando se tudo deu certo
+a<-2 
+b<-3
